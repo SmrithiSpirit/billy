b6c7eac7379eb15e322cab29dbccd254d084da44
SmrithiSpirit <45619581+SmrithiSpirit@users.noreply.github.com>
2020-11-30 20:46:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 $("#submitBtn").click(function () {
   var formData = JSON.stringify($('form').serializeArray());
+  console.log(formData)
   
   window.parent.postMessage(JSON.stringify({
     event_code: 'ym-client-event', data: JSON.stringify({
